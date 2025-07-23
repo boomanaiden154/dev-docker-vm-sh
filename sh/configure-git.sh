7dcfee02622223b749f6360b520b3b4cd2d70d6a
Aiden Grossman <aidengrossman@google.com>
2025-07-23 13:51:45 +0000

    Update my email address
    I am using my work email most of these for committing, so switch this script
    over to that.

--- sh/configure-git.sh
+++ sh/configure-git.sh
@@ -1,4 +1,4 @@
 git config --global user.name "Aiden Grossman"
-git config --global user.email "agrossman154@yahoo.com"
+git config --global user.email "aidengrossman@google.com"
 git config --global credential.helper store
 git config --global core.editor vim
